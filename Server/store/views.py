e9bbc6307dbd5cfc45e45a42e3dee93cbed4b7f6
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-13 11:45:06 +0530

    List Generic view

--- Server/store/views.py
+++ Server/store/views.py
@@ -1,3 +1,8 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Movie
 
 # Create your views here.
+class MovieList(ListView):
+    model = Movie
